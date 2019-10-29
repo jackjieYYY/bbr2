f65a45bcc98c632ee6dd69164792ad9d20b4dc6b
jackjieS <563255057@qq.com>
2019-10-29 23:42:56 +1100

    111

--- bbr2.sh
+++ bbr2.sh
@@ -63,36 +63,9 @@ debian_install(){
 	fi
 }
 
-start(){
-while :
-do
-while :
-do
-    echo "0) Exit script. | 退出腳本。 (0"
-    if [[ "$environment_headers" != "true" ]] || [[ "$environment_image" != "true" ]]; then echo "1) Install the kernel for BBR2. | 安裝適用於BBR2的內核。 (1"; fi
-    [[ "$environment_kernel" = "true" ]] && [[ "$environment_bbr2" != "true" ]] && echo "2) Enable BBR2. | 啟用BBR2。 (2"
-    [[ "$environment_bbr2" = "true" ]] && echo "3) Disable BBR2. | 禁用BBR2。 (3"
-    [[ "$environment_bbr2" = "true" ]] && [[ "$environment_ecn" != "true" ]] && echo "4) Enable ECN. | 啟用ECN。 (4"
-    [[ "$environment_ecn" = "true" ]] && echo "5) Disable ECN. | 禁用ECN。 (5"
-    [[ "$environment_headers" = "true" ]] && [[ "$environment_image" = "true" ]] && [[ "$environment_otherkernels" = "true" ]] && echo "6) Remove other kernels. | 卸載其餘內核。 (6"
-    [[ "$environment_headers" = "true" ]] && [[ "$environment_image" = "true" ]] && [[ "$environment_kernel" != "true" ]] && echo "7) reboot. | 重新啟動。 (7"
-    unset choose_an_option
-    read -p "Choose an option. | 選擇一個選項。 (Input a number | 輸入一個數字) " choose_an_option
-
-    if [[ "$choose_an_option" = "0" ]] || [[ "$choose_an_option" = "1" ]] || [[ "$choose_an_option" = "2" ]] || [[ "$choose_an_option" = "3" ]] || [[ "$choose_an_option" = "4" ]] || [[ "$choose_an_option" = "5" ]] || [[ "$choose_an_option" = "6" ]] || [[ "$choose_an_option" = "7" ]]; then
-        do_option $choose_an_option
-        break
-    else
-        continue
-    fi
-done
-
-done
-}
-
-
-
 start_menu(){
+	echo "当前算法 : "
+	sysctl net.ipv4.tcp_congestion_control
 	read -p "请输入数字(1/2/3)  1：安装BBRv2  2：开启ECN  3：我是咸鱼我退出:" num
 	case "$num" in
 		1)
@@ -106,5 +79,4 @@ start_menu(){
 		;;
 	esac
 }
-start
 start_menu
